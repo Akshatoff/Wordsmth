1d5921cf0a71fadf447a0a32c5a0ec8ff51d245e
Akshatoff <akshatascoder@gmail.com>
2024-04-14 17:46:09 +0530

    Updated

--- script/script.js
+++ script/script.js
@@ -1,13 +1,7 @@
-new fullpage("#fullpage", {
-    autoScrolling:true,
-	navigation: true,
-	navigationPosition: 'right',
-	navigationTooltips: ['Home', 'About', "Product"],
-
-})
 
 $(".option").click(function(){
-   $(".option").removeClass("active");
-   $(this).addClass("active");
-   
-});
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
+
