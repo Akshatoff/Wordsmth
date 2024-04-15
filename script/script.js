508ebe41e0d4966b1d26e9da0d81eee38fb6a68f
Akshatoff <akshatascoder@gmail.com>
2024-04-15 19:28:10 +0530

    Update

--- script/script.js
+++ script/script.js
@@ -1,6 +1,35 @@
+const heading = document.getElementById("heading");
+
+
+function fadeIn(element) {
+    const timeline = anime.timeline({
+      easing: 'easeOutExpo',
+      duration: 2000,
+    });
+  
+    timeline.add({
+      targets: element,
+      opacity: [0, 1],
+      translateY: [-100, 0],
+    });
+  
+    timeline.play();
+  }
+  
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        fadeIn("#heading");
+      }, 1000);
+}) 
+
+  
+
 new fullpage("#fullpage", {
     autoScrolling:true,
     navigation: false,
+    anchors:['homesec', 'aboutsec', 'productsec', 'teamsec'],
+    lockAnchors:true,
+    licenseKey: 'gplv3-license',
 })
 
 $(".option").click(function(){
